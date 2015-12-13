c0f3decd0f0f93cc47ad012da107adefabfc86e3
mxqiao <mxqiao@thoughtworks.com>
2015-12-13 12:46:15 +0800

    [qmx] init line processer

---
+++ LineProcessorInCyberDojo/LineProcesser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineProcessorInCyberDojo
+{
+    //03DA6D 
+    public class LineProcesser
+    {
+        internal static string Wrapper(string input, int columnNum)
+        {
+            var words = CollectWords(input);
+            return FormatOutput(columnNum, words);
+        }
+
+        private static string FormatOutput(int columnNum, List<string> words)
+        {
+            var result = new StringBuilder();
+            var newLine = new StringBuilder();
+
+            foreach (var word in words)
+            {
+                if (newLine.Length + word.Length <= columnNum || !Char.IsLetterOrDigit(word[0]))
+                {
+                    newLine.Append(word);
+                }
+                else
+                {
+                    result.Append(newLine.ToString() + "\r\n");
+                    newLine = new StringBuilder();
+                    newLine.Append(word);
+                }
+            }
+            result.Append(newLine.ToString());
+
+            return result.ToString();
+        }
+
+        private static List<string> CollectWords(string input)
+        {
+            if (input.Equals(string.Empty)) return new List<string>() {input};
+
+            var words = new List<string>();
+            var word = input[0].ToString();
+            for (var i = 1; i < input.Length; i++)
+            {
+                var letter = input[i];
+                if (char.IsLetterOrDigit(letter))
+                {
+                    word = word.Insert(word.Length, letter.ToString());
+                }
+                else
+                {
+                    words.Add(word);
+                    words.Add(letter.ToString());
+                    word = "";
+                }
+            }
+            if (Char.IsLetterOrDigit(input[input.Length - 1]))
+            {
+                words.Add(word);
+            }
+            return words;
+        }
+    }
+}
---
+++ LineProcessorInCyberDojo/LineProcesserTest.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+
+namespace LineProcessorInCyberDojo
+{
+    public class LineProcesserTest
+    {
+        [Fact]
+        public void should_print_origin_string_when_string_is_not_greater_than_column_number()
+        {
+            string input = "Test print string";
+            int columnNum = 17;
+
+            Assert.Equal(input, LineProcesser.Wrapper(input, columnNum));
+        }
+        
+        [Fact]
+        public void should_print_empty_string_when_string_empty()
+        {
+            string input = "";
+            int columnNum = 17;
+
+            Assert.Equal(input, LineProcesser.Wrapper(input, columnNum));
+        }
+        
+        [Fact]
+        public void should_format_string_when_string_contain_number()
+        {
+            string input = "Test print string1 twice.";
+            int columnNum = 18;
+
+            Assert.Equal("Test print string1 \r\ntwice.", LineProcesser.Wrapper(input, columnNum));
+        }
+        
+
+        [Fact]
+        public void should_print_string_in_two_line_when_string_contain_one_more_word()
+        {
+            string input = "Test print string twice.";
+            int columnNum = 18;
+
+            Assert.Equal("Test print string \r\ntwice.", LineProcesser.Wrapper(input, columnNum));
+        }
+
+        [Fact]
+        public void should_format_string_when_string_need_three_lines()
+        {
+            string input = "Test print string1 twice. Thought.";
+            int columnNum = 10;
+
+            Assert.Equal("Test print \r\nstring1 \r\ntwice. \r\nThought.", LineProcesser.Wrapper(input, columnNum));
+        }
+    }
+}
