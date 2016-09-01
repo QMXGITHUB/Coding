0aa72b9c29d77a3ab3757427b95fa054643a0bc2
QMXGITHUB <mxqiao@thoughtworks.com>
2016-09-01 08:27:34 +0800

    new person class with static method

---
+++ CodeSmell-StaticMethod/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeSmell_StaticMethod
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static Person CreateNew()
+        {
+            var person = new Person();
+            person.Id = 1;
+            person.FirstName = "Meixia";
+            person.LastName = "Qiao";
+            return person;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}", FirstName, LastName);
+        }
+    }
+}
---
+++ CodeSmell-StaticMethod/PersonApp.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeSmell_StaticMethod
+{
+    public class PersonApp
+    {
+        public void DoSomething()
+        {
+            var person = Person.CreateNew();
+            person.FirstName = "NewName";
+            person.LastName = "NewFamilyName";
+            System.Console.WriteLine(person.ToString());
+        }
+    }
+}
